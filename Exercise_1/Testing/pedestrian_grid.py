03565a36009e93477fc3293b364d2304d020e665
juliusabelson <julius.abelson@gmail.com>
2021-04-18 20:15:24 +0200

    Can't move ped out of grid added

--- Exercise_1/Testing/pedestrian_grid.py
+++ Exercise_1/Testing/pedestrian_grid.py
@@ -7,16 +7,35 @@ import itertools
 import operator
 
 class PedestrianGrid():
+    '''
+    Main class for simulating the crowd
+    '''
 
     def __init__(self, n, m, pedestrians, targets, obstacles = None):
+        '''
+        Initializes the grid on which to simulate the crowd movements as a numpy array with
+        free space (coded as 0), pedestrians (coded as 1), targets (coded as 2) and
+        obstacles (coded as 3).
+
+
+        :param n: (int) Width of the grid
+        :param m: (int) Height of the grid
+        :param pedestrians: (list) List of tuples (int, int) with coordinates for the pedestrians
+        :param targets: (list) List of tuples (int, int) with coordinates for the targets
+        :param obstacles: (list) List of tuples (int, int) with coordinates for the obstacles
+        '''
+        
         self.size = n, m
         self.pedestrians = pedestrians
         self.targets = targets
         self.obstacles = obstacles
         self.grid = self.__generate_grid()
 
-
     def __generate_grid(self):
+        '''
+        Initialize the grid and mark the cells accordingly
+        '''
+
         tmp_grid = np.zeros((self.size))
 
         for pedestrian in self.pedestrians:
@@ -32,6 +51,15 @@ class PedestrianGrid():
         return tmp_grid
 
     def move_pedestrians(self, moving_instructions):
+        ''' 
+        Moves the pedestrians in accordance with the moving_instructions.
+
+        :param moving_instructions: (list) List of tuples (int, int) containing how much the corresponding 
+                                    pedestrian is to move. 
+                                    Example: moving_instructions[0] = (1,1) would move pedestrians[0]
+                                    one cell in each direction.
+        '''
+
         if len(moving_instructions) is not len(self.pedestrians):
             print(f'Error in length of the moving instructions, which has to match number of pedestrians. Length of list is {len(moving_instructions)}, expected {len(self.pedestrians)}')
 
@@ -41,12 +69,19 @@ class PedestrianGrid():
                 pedestrian = self.pedestrians[i]
 
                 self.grid[pedestrian] = 0
-                pedestrian = tuple(map(operator.add, move, pedestrian))
-                self.pedestrians[i] = pedestrian
-                self.grid[pedestrian] = 1
-
+                new_pedestrian = tuple(map(operator.add, move, pedestrian))
+                if new_pedestrian[0] < self.size[0] and new_pedestrian[1] < self.size[1]:
+                    self.pedestrians[i] = new_pedestrian
+                    self.grid[new_pedestrian] = 1
+                else:
+                    self.grid[pedestrian] = 1
+                    print(f'Can\'t move pedestrian {i} outside the grid, therefore kept at original position')
 
     def plot_grid(self):
+        '''
+        Plots the grid
+        '''
+
         colors = ['white', 'green', 'black', 'lightblue']
         plt.pcolormesh(self.grid, cmap=LinearSegmentedColormap.from_list('', colors))
         legend_elements = [Patch(facecolor=color, edgecolor='black') for color in colors]

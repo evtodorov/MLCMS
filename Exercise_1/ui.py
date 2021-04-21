98e3fc0bdbb28f29ce78111218fe753606d30b2b
etodorov_tum <evgeni.todorov@tum.de>
2021-04-21 17:53:44 +0100

    Improve UI to handle simulation and animation parameters

--- Exercise_1/ui.py
+++ Exercise_1/ui.py
@@ -6,8 +6,9 @@ Created on Sun Apr 18 09:59:46 2021
 
 @author: etodorov
 """
-import os, glob
+import os, io, glob
 from datetime import datetime
+import yaml
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import colors
@@ -25,12 +26,26 @@ class UI(object):
     """
     def __init__(self, max_frames =  100):
         """
-        Initialize a User interface object
+        Initialize a default User interface object
 
         :param max_frames: (int)
             Maximum frames in the simulation
         """
+        self.animation = {}
         self.max_frames = max_frames
+        self.animation["frame_per_second"] = 10
+        self.animation["time_steps_per_frame"] = 1
+        self.animation["save"] = True
+        self.animation["result_dir"] = RESULT_DIR
+        
+        self.simulation = {}
+        self.simulation["cell_size"] = 1.0 #m
+        self.simulation["time_step"] = 1.0 #s
+        self.simulation["duration"] = float(max_frames)
+        self.simulation["population"] = "clones"
+        self.simulation["seed"] = 42
+        self.simulation["algorithm"] = "basic"
+        
     
     def start_CLI(self):
         '''
@@ -39,7 +54,7 @@ class UI(object):
         self.intro()
         gridarray = self.get_init_condition()
         self.grid = PedestrianGrid(gridarray)
-        self.animate()
+        self.animate(self.animation['frame_per_second'])
         self.exit()
         
     def intro(self):
@@ -56,12 +71,15 @@ class UI(object):
 
     def get_init_condition(self, ic_pattern=INIT_CONDITIONS_PATTERN):
         """
-        Establish the initial condition from a choice of files
+        Establish configuration and initial condition from a choice of files
         
         :param ic_pattern: (string)
             (optional) - Glob pattern of initial condition files
         
-        :return: (np.array)
+        :return gridarray: (np.array)
+            Initial condition for a PedestrianGrid
+        :return options: (dict)
+            Configuration options from file
         """
         print("Choose initial conditions:\n"
               "The following initial conditions match ", ic_pattern,
@@ -95,8 +113,9 @@ class UI(object):
                 self.icname, _ = os.path.splitext(
                                     os.path.basename(filelist[n-1]))
                 gridarray = self.file2grid(filelist[n-1])
-            except ValueError as e:
+            except (IOError, ValueError, yaml.YAMLError) as e:
                 print("Error reading the file:\n\t", str(e))
+                raise e
                 return self.get_init_condition(ic_pattern)
             return gridarray
     
@@ -107,9 +126,13 @@ class UI(object):
         :param fname: (string)
             file path of the file with initial conditions
         
-        :return: np.array
+        :return: (np.array)
+            Initial condition for a PedestrianGrid
         """
-        gridarray = np.genfromtxt(fname, dtype=int, delimiter=1)
+        with open(fname, 'r') as stream:
+            config = yaml.safe_load(stream)
+        initial_condition = self.parse_config(config)
+        gridarray = np.genfromtxt(initial_condition, dtype=int, delimiter=1)
         if (gridarray==1).sum() < 1:
             raise ValueError("No pedestrians found in " + fname)
         if (gridarray==2).sum() != 1:
@@ -117,6 +140,47 @@ class UI(object):
         if gridarray.min() < 0 or gridarray.max() > 3:
             raise ValueError("Only values 0, 1, 2, 3 are allowed.")
         return gridarray
+    
+    def parse_config(self, config):
+        """
+        Apply configuration parameters from file
+        
+        :param config: (dict)
+            Dictionary of parameters. If any are missing, defaults are used.
+        
+        :return: (StringIO)
+            Initial conditions string to be further parsed
+        """
+        animold = self.animation
+        animnew = config["animation"]
+        for key, val in animold.items():
+            try:#basic presence and type check
+                if animnew[key] is None:
+                    raise KeyError;
+                animold[key] = type(animold[key])(animnew[key])
+            except KeyError:
+                print(f"No animation.{key} found - default {animold[key]}")
+            except (ValueError, TypeError):
+                print(f"Error with animation.{key} - default {animold[key]}")
+        
+        simold = self.simulation
+        simnew = config["simulation"]
+        for key, val in simold.items():
+            try:#basic presence and type check
+                if simnew[key] is None:
+                    raise KeyError;
+                simold[key] = type(simold[key])(simnew[key])
+            except KeyError:
+                print(f"No simulation.{key} found - default {simold[key]}")
+            except (ValueError, TypeError):
+                print(f"Error with simulation.{key} - default {simold[key]}")
+            
+        try: 
+            initial_condition = simnew["initial_condition"]
+        except KeyError:
+            raise ValueError("The file must contain initial condition!")
+        
+        return io.StringIO(initial_condition)
     
     def plot(self, grid):
         """
@@ -128,7 +192,8 @@ class UI(object):
         :return (fig, ax, im): (tuple)
             Plotted figure, axis and image objects
         """
-        fig = plt.figure(figsize=(10,10))
+        fig = plt.figure(figsize=(self.grid.size[0]/10+2,
+                                  self.grid.size[1]/10+1))
         ax = plt.gca()
         if grid.max() == 3:
             self.colormap = colors.ListedColormap(['white','green', 'yellow', 'red'])
@@ -160,21 +225,38 @@ class UI(object):
             Frames per second of animation
         """
         self.fig, self.ax, self.im = self.plot(self.grid.grid)
-        ani = FuncAnimation(self.fig, self.draw, frames=self.max_frames)
+        ani = FuncAnimation(self.fig, 
+                            self.draw,
+                            fargs = (self.animation['time_steps_per_frame'],),
+                            frames=self.max_frames)
         timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
-        ani.save(os.path.join(RESULT_DIR, f"{self.icname}_{timestamp}.gif"),
-                 writer = PillowWriter(fps=fps))                             
+        if self.animation["save"]:
+            ani.save(os.path.join(self.animation["result_dir"],
+                                  f"{self.icname}_{timestamp}.gif"),
+                     writer = PillowWriter(fps=fps))  
+        else:
+            plt.show()                           
         
-    def draw(self,i):
+    def draw(self,i, tpf=1):
         """
         Draw the next frame of the animation
         
         :param i: (int)
             Frame number
+        :param tpf: (int)
+            Frames to run before drawing
+            
+        :return im: (plt.Image)
+        :return ax.time_text: (plt.Text)
         """
-        self.grid.evolve()
+        if tpf==1:
+            self.grid.evolve()
+        else:
+            self.grid.simulate(tpf)
+            
         self.im.set_data(self.grid.grid)
-        self.ax.time_text.set_text(f"frame = {i}" )
+        dt = self.simulation['time_step']
+        self.ax.time_text.set_text(f"frame = {i}; t = {i*tpf*dt} s" )
         return self.im, self.ax.time_text
     
     def exit(self):

ff53a1cf3d34111af1b871c13cc1a03c81f9efb9
etodorov_tum <evgeni.todorov@tum.de>
2021-05-07 20:24:48 +0200

    Sort out visualisation

--- Exercise_2/SIRvisualization/utils.py
+++ Exercise_2/SIRvisualization/utils.py
@@ -7,29 +7,28 @@ import plotly.graph_objects as go
 
 def file_df_to_count_df(df,
                         ID_SUSCEPTIBLE=1,
-                        ID_INFECTED=0):
+                        ID_INFECTED=0,
+                        ID_REMOVED=2):
     """
     Converts the file DataFrame to a group count DataFrame that can be plotted.
     The ID_SUSCEPTIBLE and ID_INFECTED specify which ids the groups have in the Vadere processor file.
     """
-    pedestrian_ids = df['pedestrianId'].unique()
-    sim_times = df['simTime'].unique()
+    #pedestrian_ids = df['pedestrianId'].unique()
+    sim_times = pd.np.arange(0,round(df['simTime'].max()))
     group_counts = pd.DataFrame(columns=['simTime', 'group-s', 'group-i', 'group-r'])
     group_counts['simTime'] = sim_times
-    group_counts['group-s'] = 0
-    group_counts['group-i'] = 0
-    group_counts['group-r'] = 0
-
-    for pid in pedestrian_ids:
-        simtime_group = df[df['pedestrianId'] == pid][['simTime', 'groupId-PID5']].values
-        current_state = ID_SUSCEPTIBLE
-        group_counts.loc[group_counts['simTime'] >= 0, 'group-s'] += 1
-        for (st, g) in simtime_group:
-            if g != current_state and g == ID_INFECTED and current_state == ID_SUSCEPTIBLE:
-                current_state = g
-                group_counts.loc[group_counts['simTime'] > st, 'group-s'] -= 1
-                group_counts.loc[group_counts['simTime'] > st, 'group-i'] += 1
-                break
+    df['simTime_datetime'] = pd.to_datetime(df['simTime'], unit='s') 
+    binned_groups = df.groupby([pd.Grouper(key='simTime_datetime',freq='1s'), pd.Grouper('pedestrianId')]).max()
+    try:
+        valcounts = binned_groups.groupby('simTime_datetime')['groupId-PID5'].value_counts()
+    except KeyError: 
+        valcounts = binned_groups.groupby('simTime_datetime')['groupId-PID9'].value_counts()
+    valcounts.index = valcounts.index.set_levels(valcounts.index.levels[0].astype(int).astype(float)/1000000000, level=0)    
+    for name, id in {'group-s':ID_SUSCEPTIBLE, 'group-i':ID_INFECTED, 'group-r': ID_REMOVED}.items():
+        try:
+            group_counts[name] = valcounts.loc[:,id]
+        except KeyError:
+            group_counts[name] = 0
     return group_counts
 
 
@@ -64,7 +63,7 @@ def create_folder_data_scatter(folder):
         return [scatter_s, scatter_i], group_counts
     else:
         scatter_r = go.Scatter(x=group_counts['simTime'],
-                               y=group_counts['group-i']+group_counts['group-s']+group_counts['group-i'],
+                               y=group_counts['group-r']+group_counts['group-s']+group_counts['group-i'],
                                name='recovered ' + os.path.basename(folder),
                                mode='lines', fill='tozeroy')
         scatter_s = go.Scatter(x=group_counts['simTime'],

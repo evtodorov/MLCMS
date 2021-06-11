2c5aa8911bf475c140741e2e08e1268c84d9c3b1
juliusabelson <julius.abelson@gmail.com>
2021-06-11 17:07:48 +0200

    Task one (kind of) finished

--- Exercise_5/Task_1/helper_functions.py
+++ Exercise_5/Task_1/helper_functions.py
@@ -56,11 +56,80 @@ def linear_fit(x, f):
 
     '''
 
-    A = np.vstack([x.T, np.ones(len(x))]).T
-    k, m = np.linalg.lstsq(A, f, rcond=None)[0]
-    print(f'Got coefficients for the fit f_hat(x) = k*x + m as \n k = {k[0].round(3)}, m = {m[0].round(3)}')
-    return k, m
+    A = np.vstack([x.T, np.ones(x.T.shape)]).T
+    coeff = np.linalg.lstsq(A, f, rcond=None)[0]
+    print(f'Got coefficients for the fit f_hat(x) = k*x + m, as \nk = {coeff[0][0].round(3)}, m = {coeff[1][0].round(3)}')
+    f_hat = coeff[0]*x + coeff[1]
+
+    return f_hat
 
 
-def non_linear_fit(x, f):
-    pass
+def rbf_calc(x_l, x, eps):
+    '''
+    Calculates the radial basis function
+
+    Input:
+        :param x_l: (float)
+            Centre of the basis function
+
+        :param x: (numpy array np.shape = (dim, 1)))
+            x values
+        
+        :param epsilon: (float)
+            The bandwidth of the basis function
+        
+    Returns:
+        :param rbf: (numpy array np.shape = (dim, 1)))
+            Radial basis function of x, with parameters x_l and eps.
+
+    '''
+
+    r = (x_l - x)**2
+    rbf = np.exp(-r/(eps**2))
+
+    return rbf
+
+
+def non_linear_fit(x, f, l, eps):
+    '''
+    Makes a nonlinear fit to f(x) based on the values of x,
+    i.e. f(x) ~ f_hat(x) =  Σ cΦ
+
+    Input:
+        :param x: (numpy array np.shape = (dim, 1)))
+            x values
+
+        :param f: (numpy array np.shape = (dim, 1)))
+            Function to be fitted
+
+        :param l: (int)
+            The number of radial basis functions to be combined
+
+        :param epsilon: (float)
+            The bandwidth of the basis function
+        
+        
+    Returns:
+        :param rbf: (numpy array np.shape = (dim, 1)))
+            Radial basis function of x, with parameters x_l and eps.
+
+    '''
+    
+    N = x.shape[0]
+
+    # Create evenly distributed x_ls
+    x_ls = np.linspace(0, np.max(x), l).reshape(l,1) 
+
+    # Fill a list with phis
+    phi_sums = [rbf_calc(x_l, x, eps) for x_l in x_ls]
+    phi_sums = np.array(phi_sums).reshape(l, N)
+    
+    # Get the coefficient
+    A = np.vstack([phi_sums, np.ones(phi_sums.shape)]).T
+    coeff = np.linalg.lstsq(A, f, rcond=None)[0]
+
+    # Sum up all the radial basis functions
+    func_lst = [rbf_calc(x_l, x, eps)*coeff[i] for i, x_l in enumerate(x_ls)]
+    f_hat = np.sum(np.array(func_lst), axis=0)
+
+    return f_hat

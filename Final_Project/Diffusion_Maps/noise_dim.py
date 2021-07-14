c5ed2b865d41eeac4534dc779a2ee10fa1624bf1
Ajo George <ge59luc>
2021-07-14 16:09:53 +0200

    add comment to noise_dim.py

--- Final_Project/Diffusion_Maps/noise_dim.py
+++ Final_Project/Diffusion_Maps/noise_dim.py
@@ -1,6 +1,17 @@
 import numpy as np
 
 def add_noise_dim(orig_data, no_noise_dim):
+    """
+        Function that adds the required number of noise dimensions to the set and gives a
+        random rotation to the matrix
+
+        :param orig_data: (np.array with np.shape = (N, 3))
+            The original data
+
+        :param no_noise_dim: (int)
+            Number of noise dimensions you want to add to the set.
+
+        """
 
     no_data_points  = orig_data.shape[0]
     no_dim_orig_data = orig_data.shape[1]
